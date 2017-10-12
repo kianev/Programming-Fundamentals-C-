d088bf78a3fb103d730b666beb515bb0ed86e7a9
Kaloian Ianev <kianev@gmail.com>
2017-10-12 09:04:45 +0200

    solutions

---
+++ 07.Programming-Fundamentals-Lists-Exercises/07.BombNumbers/07.BombNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.BombNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int[] bombParameters = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bombParameters[0])
+                {
+                    for (int j = 1; j <= bombParameters[1]; j++)
+                    {
+                        if (i - j < 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            numbers[i - j] = 0;
+                        }
+                    }
+
+                    for (int j = 1; j <= bombParameters[1]; j++)
+                    {
+                        if (i + j > numbers.Count - 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            numbers[i + j] = 0;
+                        }
+                    }
+
+                    numbers[i] = 0;
+                }
+            }
+
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 07.Programming-Fundamentals-Lists-Exercises/6.SumReversedNumbers/6.SumReversedNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.SumReversedNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> elements = Console.ReadLine().Split(' ').ToList();
+
+            List<string> reversed = new List<string>();
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                char[] inputElement = elements[i].ToCharArray();
+                Array.Reverse(inputElement);
+                string outputElement = new string(inputElement);
+
+                reversed.Add(outputElement);
+            }
+
+            int sum = 0;
+
+            foreach (var item in reversed)
+            {
+                int number = int.Parse(item);
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
