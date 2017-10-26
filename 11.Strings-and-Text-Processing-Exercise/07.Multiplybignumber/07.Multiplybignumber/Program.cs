4f2164e7094f1e432cf303669862336c329c31ac
Kaloian Ianev <kianev@gmail.com>
2017-10-26 09:54:38 +0200

    solutions

--- 11.Strings-and-Text-Processing-Exercise/07.Multiplybignumber/07.Multiplybignumber/Program.cs
+++ 11.Strings-and-Text-Processing-Exercise/07.Multiplybignumber/07.Multiplybignumber/Program.cs
@@ -1,20 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _07.Multiplybignumber
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            BigInteger num = BigInteger.Parse(Console.ReadLine());
-            int n = int.Parse(Console.ReadLine());
+            string first = Console.ReadLine().TrimStart(new char[] { '0' }); 
+            int second = int.Parse(Console.ReadLine().TrimStart(new char[] { '0' }));
 
+            StringBuilder sb = new StringBuilder();
+            var sum = 0;
+            var reminder = 0;
 
+            for (int i = first.Length - 1; i >= 0; i--)
+            {
+                sum = (first[i] - 48) * second + reminder;
+
+                if (sum > 9)
+                {
+                    if (i == 0)
+                    {
+                        sb.Append(sum % 10);
+                        sb.Append(sum / 10);
+                    }
+                    else
+                    {
+                        reminder = sum / 10;
+                        sum = sum % 10;
+                        sb.Append(sum);
+                    }
+                }
+                else
+                {
+                    reminder = 0;
+                    sb.Append(sum);
+                }
+            }
+
+            string reversed = new string(sb.ToString().ToCharArray().Reverse().ToArray()).TrimStart('0');
+            Console.WriteLine(reversed);
         }
     }
 }
---
+++ 11.Strings-and-Text-Processing-Exercise/08.LettersChangeNumbers/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.LettersChangeNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] {' ', '\t'},
+                StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            double sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char first = Convert.ToChar(input[i].Substring(0, 1));
+                double num = double.Parse(input[i].Substring(1, input[i].Length - 2));
+                char last = Convert.ToChar(input[i].Substring(input[i].Length - 1, 1));
+
+                if (char.IsUpper(first))
+                {
+                    var firstPosition = first - 'A' + 1;
+                    sum += num / firstPosition;
+                }
+                else
+                {
+                    var firstPosition = first - 'a' + 1;
+                    sum += num * firstPosition;
+                }
+
+                if (char.IsUpper(last))
+                {
+                    var lastPosition = last - 'A' + 1;
+                    sum -= lastPosition;
+                }
+                else
+                {
+                    var lastPosition = last - 'a' + 1;
+                    sum += lastPosition;
+                }
+            }
+
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
